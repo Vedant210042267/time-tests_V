c51422f09b51d4e6f4ea3f7aaa886dacaa217036
Xiaoheng <2xh18964858612@gmail.com>
2025-10-27 09:43:49 +0000

    Add multiple intervals unit test case

--- test_times.py
+++ test_times.py
@@ -11,3 +11,16 @@ def test_given_input():
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
 
     assert result == expected
+
+
+# multiple intervals in both ranges
+def multiple_intervals():
+    range1 = time_range("2022-05-01 09:00:00", "2022-05-01 11:00:00", 3, 30)
+    range2 = time_range("2022-05-01 10:15:00", "2022-05-01 12:15:00", 4, 15)
+
+    result = compute_overlap_time(range1, range2)
+
+    expected = [('2022-05-01 10:15:00', '2022-05-01 10:30:00'), ('2022-05-01 10:45:00', '2022-05-01 11:00:00')]
+
+    assert result == expected
+
